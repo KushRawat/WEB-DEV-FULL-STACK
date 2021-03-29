e3fa0d165bf3120064cfe90e907a8040d8572610
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-03-29 13:34:54 +0530

    nested loops

--- appp.js
+++ appp.js
@@ -4,12 +4,25 @@
 //     console.log(random)
 // }
 
-const dayOfWeek = 'Friday';
+// const dayOfWeek = 'Friday';
 
-if (dayOfWeek === 'Monday') {
-    console.log("UGGHH I hate mondays")
-} else if (dayOfWeek === 'Saturday') {
-    console.log('Yay I Love Saturdays! ')
-} else if (dayOfWeek === 'Friday') {
-    console.log('Fridays are decent. Especially after work')
-} 
+// if (dayOfWeek === 'Monday') {
+//     console.log("UGGHH I hate mondays")
+// } else if (dayOfWeek === 'Saturday') {
+//     console.log('Yay I Love Saturdays! ')
+// } else if (dayOfWeek === 'Friday') {
+//     console.log('Fridays are decent. Especially after work')
+// } 
+
+const password = prompt("Please enter a new password");
+
+if (password.length >= 6) {
+    if (password.indexOf(' ') === -1) {
+        console.log("Valid password")
+    } else {
+        console.log("Password cannot contain spaces")
+    }
+
+} else {
+    console.log("PASSWORD TOO SHORT! MUST BE 6+ CHARACTERS")
+}

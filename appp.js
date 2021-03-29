59a9346cd5865b2f98040fd2c551db6017900fcc
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-03-29 15:29:22 +0530

    logical OR ||

--- appp.js
+++ appp.js
@@ -27,10 +27,28 @@
 //     console.log("PASSWORD TOO SHORT! MUST BE 6+ CHARACTERS")
 // }
 
-const password = prompt("Enter a password")
+// const password = prompt("Enter a password")
 
-if (password.length >= 6 && password.indexOf(" ") === -1) {
-    console.log("Valid Password")
+// if (password.length >= 6 && password.indexOf(" ") === -1) {
+//     console.log("Valid Password")
+// } else {
+//     console.log("Incorrect format for password")
+// }
+
+// 0-5 free
+// 5-10
+// 10-65
+// 65+
+
+
+const age = 50;
+
+if (age >= 0 && age < 5 || age >= 65) {
+    console.log("Free")
+} else if (age >= 5 && age < 10) {
+    console.log("$10")
+} else if (age >= 10 && age < 65) {
+    console.log("$20")
 } else {
-    console.log("Incorrect format for password")
+    console.log("Invalid age")
 }

65266f1c2dc2184aa43515d7107d2fe6d8dfcedc
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-03-29 11:12:24 +0530

    conditionals if else

--- appp.js
+++ appp.js
@@ -1,5 +1,15 @@
-let random = Math.random();
-if (random < 0.5) {
-    console.log("Your number is less than 0.5")
-    console.log(random)
-}
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("Your number is less than 0.5")
+//     console.log(random)
+// }
+
+const dayOfWeek = 'Friday';
+
+if (dayOfWeek === 'Monday') {
+    console.log("UGGHH I hate mondays")
+} else if (dayOfWeek === 'Saturday') {
+    console.log('Yay I Love Saturdays! ')
+} else if (dayOfWeek === 'Friday') {
+    console.log('Fridays are decent. Especially after work')
+} 

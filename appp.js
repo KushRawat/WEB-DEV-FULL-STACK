8b534a80c4dd6a7714de40022bf5620ea0d83506
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-03-29 14:07:48 +0530

    logical and

--- appp.js
+++ appp.js
@@ -14,15 +14,23 @@
 //     console.log('Fridays are decent. Especially after work')
 // } 
 
-const password = prompt("Please enter a new password");
+// const password = prompt("Please enter a new password");
 
-if (password.length >= 6) {
-    if (password.indexOf(' ') === -1) {
-        console.log("Valid password")
-    } else {
-        console.log("Password cannot contain spaces")
-    }
+// if (password.length >= 6) {
+//     if (password.indexOf(' ') === -1) {
+//         console.log("Valid password")
+//     } else {
+//         console.log("Password cannot contain spaces")
+//     }
 
+// } else {
+//     console.log("PASSWORD TOO SHORT! MUST BE 6+ CHARACTERS")
+// }
+
+const password = prompt("Enter a password")
+
+if (password.length >= 6 && password.indexOf(" ") === -1) {
+    console.log("Valid Password")
 } else {
-    console.log("PASSWORD TOO SHORT! MUST BE 6+ CHARACTERS")
+    console.log("Incorrect format for password")
 }

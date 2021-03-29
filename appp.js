f256f751197d93c8202972104d553a29460afa65
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-03-29 15:33:18 +0530

    logical NOT !

--- appp.js
+++ appp.js
@@ -41,14 +41,8 @@
 // 65+
 
 
-const age = 50;
+const age = 2;
 
-if (age >= 0 && age < 5 || age >= 65) {
-    console.log("Free")
-} else if (age >= 5 && age < 10) {
-    console.log("$10")
-} else if (age >= 10 && age < 65) {
-    console.log("$20")
-} else {
-    console.log("Invalid age")
+if (!(age >= 0 && age < 5 || age >= 65)) {
+    console.log("You are not a baby or a senior")
 }

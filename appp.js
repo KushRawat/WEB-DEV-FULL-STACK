88fc61f3da0e2f5813b111998d1c019f52168764
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-04-01 10:54:18 +0530

    for loop

--- appp.js
+++ appp.js
@@ -47,22 +47,26 @@
 //     console.log("You are not a baby or a senior")
 // }
 
-const day = 5;
-switch (day) {
-    case 1:
-        console.log("Monday");
-        break;
-    case 2:
-        console.log("Tuesday");
-        break;
-    case 3:
-        console.log("Wednesday");
-    case 4:
-        console.log("Thursday");
-    case 5:
-    case 6:
-        console.log("Weekend");
-        break;
-    default:
-        console.log("I don't know that");
+// const day = 5;
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//     case 4:
+//         console.log("Thursday");
+//     case 5: 
+//     case 6:
+//         console.log("Weekend");
+//         break;
+//     default:
+//         console.log("I don't know that");
+// }
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
 }

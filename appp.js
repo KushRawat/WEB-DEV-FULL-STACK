9bbef5b4ebdf2cdf1a1ad7e6c3448210a5aa8cde
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-04-14 22:20:16 +0530

    filter method

--- appp.js
+++ appp.js
@@ -369,6 +369,12 @@
 // console.log("GOODBYE!!!")
 
 
-const id = setInterval(() => {
-    console.log(Math.random())
-}, 2000)  
+// const id = setInterval(() => {
+//     console.log(Math.random())
+// }, 2000)  
+
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+const result = numbers.filter(n => {
+    return n < 10 
+})

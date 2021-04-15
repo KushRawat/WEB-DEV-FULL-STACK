854da21731244ddc52f2125314c8cc489daaead9
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-04-15 20:39:56 +0530

    getElementsByTagName

--- DOM/app.js
+++ DOM/app.js
@@ -1,12 +1,5 @@
-const allLinks = document.querySelectorAll('a');
+const allImages = document.getElementsByTagName('img')
 
-// for (let link of allLinks) {
-//     link.innerText = 'I AM A LINK!!!!'
-// }
-
-
-for (let link of allLinks) {
-    link.style.color = 'rgb(0, 108, 134)';
-    link.style.textDecorationColor = 'magenta';
-    link.style.textDecorationStyle = 'wavy'
+for(let img of allImages) {
+    console.log(img.src)
 }

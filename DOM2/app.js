81c7bbad5d7b58b01cc1d4643c16c70156375841
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-04-16 20:06:14 +0530

    advantage of addEventListener over onclick

--- DOM2/app.js
+++ DOM2/app.js
@@ -15,3 +15,19 @@ btn3 = document.querySelector('#v3')
 btn3.addEventListener('click', function() {
     alert("CLICKED!! ")
 })
+
+function twist () {
+    console.log("TWIST")
+} 
+
+function shout () {
+    console.log("SHOUT")
+} 
+
+const tasButton = document.querySelector('#tas')
+
+// tasButton.onclick = twist;
+// tasButton.onclick = shout;
+
+tasButton.addEventListener('click', twist)
+tasButton.addEventListener('click', shout)

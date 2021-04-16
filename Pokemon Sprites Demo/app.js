73dcf2878d441610224b305d5e0d3c475969d348
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-04-16 16:44:52 +0530

    self

--- Pokemon Sprites Demo/app.js
+++ Pokemon Sprites Demo/app.js
@@ -17,3 +17,19 @@
 //     pokemon.appendChild(label);
 //     container.appendChild(pokemon)
 // }
+
+const container = document.querySelector('#container');
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+
+for(let i = 1; i < 153; i++) {
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon')
+    const label = document.createElement('span')
+    label.innerText = `#${i}`
+    const newImg = document.createElement('img');
+    newImg.src = `${baseURL}${i}.png`
+    pokemon.appendChild(newImg)
+    pokemon.appendChild(label)
+    container.appendChild(pokemon)
+}

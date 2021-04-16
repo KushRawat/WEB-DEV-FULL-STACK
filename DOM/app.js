f0128b7d9f0b450aefc3875dbc823ce9a02e93b3
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-04-16 11:15:22 +0530

    styling

--- DOM/app.js
+++ DOM/app.js
@@ -10,14 +10,22 @@
 //     img.src = '';
 // }
 
-const links = document.querySelectorAll('p a')
+// const links = document.querySelectorAll('p a')
 
-for (let link of links) {
-    console.log(link.href)
-}
+// for (let link of links) {
+//     console.log(link.href)
+// }
 
-const links = document.querySelectorAll('a')
+// const links = document.querySelectorAll('a')
 
-for (let link of links) {
-    console.log(link)
+// for (let link of links) {
+//     console.log(link)
+// }
+
+const allLinks = document.querySelectorAll('a')
+
+for (let link of allLinks) {
+    link.style.color = 'rgb(12, 82, 3)'
+    link.style.textDecorationColor = 'solid green'
+    link.style.textDecorationStyle = 'wavy'
 }

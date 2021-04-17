b1e26d21a70f070b4fcb13f50b58ba9942e763dc
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-04-17 14:23:57 +0530

    generic function

--- Async JS/app.js
+++ Async JS/app.js
@@ -10,15 +10,35 @@
 //     document.body.style.backgroundColor = "orange"
 // }, 3000)
 
-setTimeout(() => {
-    document.body.style.backgroundColor = "red";
+// setTimeout(() => {
+//     document.body.style.backgroundColor = "red";
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = "cyan";
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = "orange"
+//         }, 1000)
+//     }, 1000)
+// }, 1000)
+
+
+
+const delayedColorChange = (newColor, delay, doNext) => {
     setTimeout(() => {
-        document.body.style.backgroundColor = "cyan";
-        setTimeout(() => {
-            document.body.style.backgroundColor = "orange"
-        }, 1000)
-    }, 1000)
-}, 1000)
+        document.body.style.backgroundColor = newColor;
+        doNext()
+    }, delay)
+}
 
-
-
+delayedColorChange('red', 1000, () => {
+    delayedColorChange('green', 1000, () => {
+        delayedColorChange('yellow', 1000, () => {
+            delayedColorChange('orange', 1000, () => {
+                delayedColorChange('blue', 1000, () => {
+                    delayedColorChange('teal', 1000, () => {
+        
+                    })
+                })
+            })
+        })  
+    })
+} )

43cd23a16ab5c49127176cea14ce5a1be2b32395
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-04-27 12:10:22 +0530

    using async function

--- Async JS/async.js
+++ Async JS/async.js
@@ -2,16 +2,32 @@
 
 // }
 
-const sing = async () => {
-    throw new Error("UH OH")
-    return 'la la la'
+
+// const sing = async () => {
+//     throw new Error("UH OH")
+//     return 'la la la'
+// }
+
+// sing()
+//     .then((data) => {
+//         console.log("Promise resolved with:", data)
+//     })
+//     .catch((err) => {
+//         console.log("OH NO, Promise Rejected")
+//         console.log(err)
+//     })
+const login = async (username, password) => {
+    if (!username || !password) throw 'Missing Credentials';
+    if (password === 'howyoudoin') return 'WELCOME';
+    throw 'Invalid Password';
 }
 
-sing()
-    .then((data) => {
-        console.log("Promise resolved with:", data)
+login('godkush','howyoudoin')
+    .then((msg) => {
+        console.log('Logged In!')
+        console.log(msg)
     })
     .catch((err) => {
-        console.log("OH NO, Promise Rejected")
+        console.log('Error!!')
         console.log(err)
     })

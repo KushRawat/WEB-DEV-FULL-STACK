b40de443527dd7c0fef75db4c7e6954075233954
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-04-28 04:49:49 +0530

    Creating a form

--- DOM2/form.js
+++ DOM2/form.js
@@ -1,5 +1,22 @@
 const tweetForm = document.querySelector('#tweetForm')
+const tweetsContainer = document.querySelector('#tweets')
+
 tweetForm.addEventListener('submit', function(e) {
-    console.log('Submit!!')
     e.preventDefault()
+    
+    const username = tweetForm.elements.username
+    const tweet = tweetForm.elements.tweet
+
+    addTweet(username.value, tweet.value)
+    username.value = ''
+    tweet.value = ''
 })
+
+const addTweet = (username, tweet) => {
+    const newTweet = document.createElement('li')
+    const bTag = document.createElement('b')
+    bTag.append(username)
+    newTweet.append(bTag)
+    newTweet.append(`- ${tweet}`)
+    tweetsContainer.append(newTweet)    
+}

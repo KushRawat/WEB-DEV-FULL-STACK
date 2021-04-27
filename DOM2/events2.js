3e0950e3f3adc14dbbee89300ffc3a42216f73e5
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-04-28 02:47:27 +0530

    key and code

--- DOM2/events2.js
+++ DOM2/events2.js
@@ -3,9 +3,10 @@ document.querySelector('button').addEventListener('click', function (holy) {
 })
 
 const input = document.querySelector('input')
-input.addEventListener('keydown', function () {
-    console.log('KEYDOWN')
+input.addEventListener('keydown', function (evt) {
+    console.log(evt.key)
+    console.log(evt.code)
 })
-input.addEventListener('keyup', function () {
-    console.log('KEYUP')
-})
+// input.addEventListener('keyup', function () {
+//     console.log('KEYUP')
+// })

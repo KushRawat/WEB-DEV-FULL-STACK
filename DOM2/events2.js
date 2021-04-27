e98a7dfe630d743b800ebe6cfc2d12094a20d3e4
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-04-27 22:39:31 +0530

    keyboard events - keyup, keydown

--- DOM2/events2.js
+++ DOM2/events2.js
@@ -1,3 +1,11 @@
 document.querySelector('button').addEventListener('click', function (holy) {
     console.log(holy)
 })
+
+const input = document.querySelector('input')
+input.addEventListener('keydown', function () {
+    console.log('KEYDOWN')
+})
+input.addEventListener('keyup', function () {
+    console.log('KEYUP')
+})

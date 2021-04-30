92006bd52908c53e1cae155918afbbb4a4a9cba5
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-04-30 19:43:02 +0530

    Joke generator using API and axios library

--- AJAX and APIs/app.js
+++ AJAX and APIs/app.js
@@ -17,10 +17,25 @@ const fetchBitcoinPrice = async () => {
 
 // fetchBitcoinPrice()
 
-const getDadJoke = async () => {
-    const config = { headers: { Accept: 'application/json' } }
-    const res = await axios.get("https://icanhazdadjoke.com/", config)
-    console.log(res.data.joke)
+const button = document.querySelector('button')
+const jokes = document.querySelector('#jokes')
+
+const addNewJoke = async() => {
+    const jokeText = await getDadJoke()
+    const newLI = document.createElement('LI')
+    newLI.append(jokeText)
+    jokes.append(newLI)
 }
 
-getDadJoke()
+const getDadJoke = async () => {
+    try {
+        const config = { headers: { Accept: 'application/json' } }
+        const res = await axios.get("https://icanhazdadjoke.com/", config)
+        return res.data.joke
+    } catch(e) {
+        return "No Jokes Available! Sorry :("
+    }
+}
+
+button.addEventListener('click', addNewJoke)
+// getDadJoke()

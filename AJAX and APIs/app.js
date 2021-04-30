9c375a85877224767993123f41af7b90fca7182e
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-04-30 19:03:54 +0530

    setting header using axios

--- AJAX and APIs/app.js
+++ AJAX and APIs/app.js
@@ -6,13 +6,21 @@
 //         console.log("ITS AN ERRROR!!!!", err)
 //     })
 
-const fetchBitcoinPrice = async() => {
+const fetchBitcoinPrice = async () => {
     try {
         const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
         console.log(res.data.ticker.price)
-    } catch(e) {
+    } catch (e) {
         console.log("ERROR!!", e)
     }
 }
 
-fetchBitcoinPrice()
+// fetchBitcoinPrice()
+
+const getDadJoke = async () => {
+    const config = { headers: { Accept: 'application/json' } }
+    const res = await axios.get("https://icanhazdadjoke.com/", config)
+    console.log(res.data.joke)
+}
+
+getDadJoke()

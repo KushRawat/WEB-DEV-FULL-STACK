782b50c062683c77341feaa7b2444ff7df87548d
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-05-04 17:58:01 +0530

    express routing using app.get, app.post

--- EXPRESS/FirstApp/index.js
+++ EXPRESS/FirstApp/index.js
@@ -1,12 +1,29 @@
 const express = require("express")
 const app = express()
 
-app.use((req, res) => {
-    console.log("WE HAVE A REQUEST!")
-    // console.dir(req)
-    // res.send("HELLO, WE GOT YOUR REQUEST! THIS IS A RESPONSE")
-    // res.send({color: 'red'})
-    res.send('<h1>This is my webpage</h1>')
+// app.use((req, res) => {
+//     console.log("WE HAVE A REQUEST!")
+//     // console.dir(req)
+//     // res.send("HELLO, WE GOT YOUR REQUEST! THIS IS A RESPONSE")
+//     // res.send({color: 'red'})  
+//     res.send('<h1>This is my webpage</h1>')
+// })
+
+app.get('/', (req, res) => {
+    res.send("This is home page!")
+})
+
+app.post('/cats', (req, res) => {
+    res.send('POST REQUEST TO /cats!!! THIS IS DIFFERENT FROM GET REQUESTS!!')
+})
+
+app.get('/cats', (req, res) => {
+    // console.log("CAT REQUEST!!")
+    res.send("MEOWW!!")
+})
+
+app.get('/dogs', (req, res) => {
+    res.send("WOOF!!")
 })
 
 app.listen(8080, () => {

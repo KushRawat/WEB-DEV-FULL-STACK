aadad13d5b01b19c231689928a198254eba443b8
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-05-04 17:18:07 +0530

    request and response objects

--- EXPRESS/FirstApp/index.js
+++ EXPRESS/FirstApp/index.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const app = express()
 
-app.use(() => {
+app.use((req, res) => {
     console.log("WE HAVE A REQUEST!")
+    // console.dir(req)
+    // res.send("HELLO, WE GOT YOUR REQUEST! THIS IS A RESPONSE")
+    // res.send({color: 'red'})
+    res.send('<h1>This is my webpage</h1>')
 })
 
 app.listen(8080, () => {

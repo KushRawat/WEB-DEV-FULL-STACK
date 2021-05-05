44ef819931cd159313573bd168debd4d613c466f
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-05-05 20:50:47 +0530

    configuring express for ejs

---
+++ EXPRESS/Templating/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    // res.send("HI")
+    res.render('home.ejs')
+})
+
+
+app.listen('3000', () => {
+    console.log("LISTENING ON PORT 3000")
+})

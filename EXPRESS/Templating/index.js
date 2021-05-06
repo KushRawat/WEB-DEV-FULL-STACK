2a26a281583fd7234c5b301a3d309e044f35c51e
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-05-06 23:33:47 +0530

    EJS syntax, passing data to template

--- EXPRESS/Templating/index.js
+++ EXPRESS/Templating/index.js
@@ -3,11 +3,16 @@ const app = express()
 const path = require('path')
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname,'/views'))
+app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res) => {
     // res.send("HI")
     res.render('home')
+})
+
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1
+    res.render('random', { num })
 })
 
 app.listen('3000', () => {

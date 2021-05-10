254896213604c3f8cf074d91beb2061a485411c6
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-05-10 19:49:31 +0530

    new comment POST

--- RESTful Routes/ExpressPost/index.js
+++ RESTful Routes/ExpressPost/index.js
@@ -35,6 +35,12 @@ app.get('/comments/new', (req, res) => {
     res.render('comments/new')
 })
 
+app.post('/comments', (req, res) => {
+    const {username, comment} = req.body
+    comments.push({username, comment})
+    res.send("It worked!!")
+})
+
 app.get('/tacos', (req, res) => {
     res.send("GET /tacos response")
 })

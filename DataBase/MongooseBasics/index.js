345d11bf16ee528647f8c466282a96202564423e
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-05-16 14:30:51 +0530

    create mongoose model

--- DataBase/MongooseBasics/index.js
+++ DataBase/MongooseBasics/index.js
@@ -7,3 +7,13 @@ mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, 
         console.log("OH NO ERROR!!")
         console.log(err)
     })
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+})
+
+const Movie = mongoose.model('Movie', movieSchema)
+const amadeus = new Movie({ title: 'Amadeus', year: 1967, score: 9.3, rating:"blah"}) 
